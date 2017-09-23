384a87987bb7eef260e55f4e01fc4ded45d4c436
Miguel Morales <mimoralea@gmail.com>
2017-09-23 13:41:33 -0700

    Add keras classifier

--- ros/src/tl_detector/light_classification/tl_classifier.py
+++ ros/src/tl_detector/light_classification/tl_classifier.py
@@ -1,9 +1,153 @@
+import cv2
+import h5py
+import rospy
+import keras.backend as K
+import numpy as np
+
 from styx_msgs.msg import TrafficLight
+
+import keras
+from keras.models import Model
+from keras.preprocessing.image import img_to_array
+from keras.layers import Input, Activation, Concatenate
+from keras.layers import Flatten, Dropout
+from keras.layers import MaxPooling2D
+from keras.layers.convolutional import Conv2D
+from keras.layers import AveragePooling2D
+
+import keras.backend as K
+
+K.set_image_dim_ordering('tf')
+
+
+def SqueezeNet(nb_classes=3, inputs=(224, 224, 3)):
+    """ Keras Implementation of SqueezeNet(arXiv 1602.07360)
+    @param nb_classes: total number of final categories
+    Arguments:
+    inputs -- shape of the input images (channel, cols, rows)
+    """
+
+    input_img = Input(shape=inputs)
+    conv1 = Conv2D(
+        96, (7, 7), activation='relu', kernel_initializer='glorot_uniform',
+        strides=(2, 2), padding='same', name='conv1')(input_img)
+    maxpool1 = MaxPooling2D(
+        pool_size=(3, 3), strides=(2, 2), name='maxpool1')(conv1)
+
+    fire2_squeeze = Conv2D(
+        16, (1, 1), activation='relu', kernel_initializer='glorot_uniform',
+        padding='same', name='fire2_squeeze')(maxpool1)
+    fire2_expand1 = Conv2D(
+        64, (1, 1), activation='relu', kernel_initializer='glorot_uniform',
+        padding='same', name='fire2_expand1')(fire2_squeeze)
+    fire2_expand2 = Conv2D(
+        64, (3, 3), activation='relu', kernel_initializer='glorot_uniform',
+        padding='same', name='fire2_expand2')(fire2_squeeze)
+    merge2 = Concatenate()([fire2_expand1, fire2_expand2])
+
+    fire3_squeeze = Conv2D(
+        16, (1, 1), activation='relu', kernel_initializer='glorot_uniform',
+        padding='same', name='fire3_squeeze')(merge2)
+    fire3_expand1 = Conv2D(
+        64, (1, 1), activation='relu', kernel_initializer='glorot_uniform',
+        padding='same', name='fire3_expand1')(fire3_squeeze)
+    fire3_expand2 = Conv2D(
+        64, (3, 3), activation='relu', kernel_initializer='glorot_uniform',
+        padding='same', name='fire3_expand2')(fire3_squeeze)
+    merge3 = Concatenate()([fire3_expand1, fire3_expand2])
+
+    fire4_squeeze = Conv2D(
+        32, (1, 1), activation='relu', kernel_initializer='glorot_uniform',
+        padding='same', name='fire4_squeeze')(merge3)
+    fire4_expand1 = Conv2D(
+        128, (1, 1), activation='relu', kernel_initializer='glorot_uniform',
+        padding='same', name='fire4_expand1')(fire4_squeeze)
+    fire4_expand2 = Conv2D(
+        128, (3, 3), activation='relu', kernel_initializer='glorot_uniform',
+        padding='same', name='fire4_expand2')(fire4_squeeze)
+    merge4 = Concatenate()([fire4_expand1, fire4_expand2])
+
+    maxpool4 = MaxPooling2D(
+        pool_size=(3, 3), strides=(2, 2), name='maxpool4')(merge4)
+
+    fire5_squeeze = Conv2D(
+        32, (1, 1), activation='relu', kernel_initializer='glorot_uniform',
+        padding='same', name='fire5_squeeze')(maxpool4)
+    fire5_expand1 = Conv2D(
+        128, (1, 1), activation='relu', kernel_initializer='glorot_uniform',
+        padding='same', name='fire5_expand1')(fire5_squeeze)
+    fire5_expand2 = Conv2D(
+        128, (3, 3), activation='relu', kernel_initializer='glorot_uniform',
+        padding='same', name='fire5_expand2')(fire5_squeeze)
+    merge5 = Concatenate()([fire5_expand1, fire5_expand2])
+
+    fire6_squeeze = Conv2D(
+        48, (1, 1), activation='relu', kernel_initializer='glorot_uniform',
+        padding='same', name='fire6_squeeze')(merge5)
+    fire6_expand1 = Conv2D(
+        192, (1, 1), activation='relu', kernel_initializer='glorot_uniform',
+        padding='same', name='fire6_expand1')(fire6_squeeze)
+    fire6_expand2 = Conv2D(
+        192, (3, 3), activation='relu', kernel_initializer='glorot_uniform',
+        padding='same', name='fire6_expand2')(fire6_squeeze)
+    merge6 = Concatenate()([fire6_expand1, fire6_expand2])
+
+    fire7_squeeze = Conv2D(
+        48, (1, 1), activation='relu', kernel_initializer='glorot_uniform',
+        padding='same', name='fire7_squeeze')(merge6)
+    fire7_expand1 = Conv2D(
+        192, (1, 1), activation='relu', kernel_initializer='glorot_uniform',
+        padding='same', name='fire7_expand1')(fire7_squeeze)
+    fire7_expand2 = Conv2D(
+        192, (3, 3), activation='relu', kernel_initializer='glorot_uniform',
+        padding='same', name='fire7_expand2')(fire7_squeeze)
+    merge7 = Concatenate()([fire7_expand1, fire7_expand2])
+
+    fire8_squeeze = Conv2D(
+        64, (1, 1), activation='relu', kernel_initializer='glorot_uniform',
+        padding='same', name='fire8_squeeze')(merge7)
+    fire8_expand1 = Conv2D(
+        256, (1, 1), activation='relu', kernel_initializer='glorot_uniform',
+        padding='same', name='fire8_expand1')(fire8_squeeze)
+    fire8_expand2 = Conv2D(
+        256, (3, 3), activation='relu', kernel_initializer='glorot_uniform',
+        padding='same', name='fire8_expand2')(fire8_squeeze)
+    merge8 = Concatenate()([fire8_expand1, fire8_expand2])
+
+    maxpool8 = MaxPooling2D(
+        pool_size=(3, 3), strides=(2, 2), name='maxpool8')(merge8)
+
+    fire9_squeeze = Conv2D(
+        64, (1, 1), activation='relu', kernel_initializer='glorot_uniform',
+        padding='same', name='fire9_squeeze')(maxpool8)
+    fire9_expand1 = Conv2D(
+        256, (1, 1), activation='relu', kernel_initializer='glorot_uniform',
+        padding='same', name='fire9_expand1')(fire9_squeeze)
+    fire9_expand2 = Conv2D(
+        256, (3, 3), activation='relu', kernel_initializer='glorot_uniform',
+        padding='same', name='fire9_expand2')(fire9_squeeze)
+    merge9 = Concatenate()([fire9_expand1, fire9_expand2])
+
+    fire9_dropout = Dropout(0.5, name='fire9_dropout')(merge9)
+    conv10 = Conv2D(
+        nb_classes, (1, 1), kernel_initializer='glorot_uniform',
+        padding='valid', name='conv10')(fire9_dropout)
+
+    # The size should match the output of conv10
+    avgpool10 = AveragePooling2D((13, 13), name='avgpool10')(conv10)
+    flatten = Flatten(name='flatten')(avgpool10)
+    softmax = Activation("softmax", name='softmax')(flatten)
+
+    return Model(inputs=input_img, outputs=softmax)
+
+
 
 class TLClassifier(object):
     def __init__(self):
-        #TODO load classifier
-        pass
+        self.model = SqueezeNet()
+        self.model.load_weights("light_classification/weights/01.weights")
+        self.model._make_predict_function()
+        rospy.logerr("Model loaded.")
 
     def get_classification(self, image):
         """Determines the color of the traffic light in the image
@@ -15,5 +159,27 @@ class TLClassifier(object):
             int: ID of traffic light color (specified in styx_msgs/TrafficLight)
 
         """
-        #TODO implement light color prediction
+        cv2.imshow("Camera stream", image)
+        cv2.waitKey(1)
+        image = img_to_array(image)
+        image /= 255.0
+        image = np.expand_dims(image, axis=0)
+        preds = self.model.predict(image)[0]
+        pred = np.argmax(preds)
+        # 0:= No traffic light in driving direction
+        # 1:= Red traffic light in driving direction
+        # 2:= Green traffic light in driving direction
+        
+        # uint8 UNKNOWN=4
+        # uint8 GREEN=2
+        # uint8 YELLOW=1
+        # uint8 RED=0
+        rospy.logerr(preds)
+        rospy.logerr(pred)
+        
+        if pred == 1:
+            return TrafficLight.RED
+        if pred == 2:
+            return TrafficLight.GREEN
+
         return TrafficLight.UNKNOWN
